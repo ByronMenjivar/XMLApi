68d2a4a4cb175cfd5f9522dc3679da62a5092d23
Byron Wilfredo Menjivar Melendez <byron.menjivar@comedica.com.sv>
2024-11-07 18:17:41 -0600

    git add
    Commit inicial

---
+++ XMLApi/xmlapi/src/main/java/com/genaro1024/xmlapi/xmlapi/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.genaro1024.xmlapi.xmlapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api")
+@CrossOrigin(origins="*")
+public class HelloController {
+    @GetMapping("hello")
+    public ResponseEntity<?> getHello(){
+        return ResponseEntity.ok("hello");
+    }
+}
+
